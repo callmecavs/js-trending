b5912a3e1cbd64dc423d45f32dff79d7a78830aa
Michael Cavalea <callmecavs@gmail.com>
2017-01-19 15:28:48 -0500

    finish up

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { send } = require('micro')
 const xray = require('x-ray')()
 
 const selectors = {
@@ -9,12 +10,42 @@ const selectors = {
   today: 'div:nth-child(4) .float-right'
 }
 
-const cleanup = (error, result) => {
+const cleanup = res => (error, list) => {
+  if (error) {
+    send(res, 500, error)
+  }
 
+  const cleaned = list.map(item => {
+    const parsed = {}
+
+    // trimmed
+    Object
+      .keys(item)
+      .map(key => {
+        parsed[key] = item[key].trim()
+      })
+
+    // extract user and name from full
+    const [
+      user,
+      name
+    ] = parsed.full.split(' / ')
+
+    parsed.user = user
+    parsed.name = name
+
+    // remove "stars today" from today
+    parsed.today = parsed.today.split(' ')[0]
+
+    return parsed
+  })
+
+  // send cleaned repos
+  send(res, 200, cleaned)
 }
 
 const scrape = async (req, res) => {
-  xray('https://github.com/trending/javascript', '.repo-list li', [selectors])(cleanup)
+  xray('https://github.com/trending/javascript', '.repo-list li', [selectors])(cleanup(res))
 }
 
 module.exports = scrape
