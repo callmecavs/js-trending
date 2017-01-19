a0ed423082718dae08739510bbf3420b70da90ef
Michael Cavalea <callmecavs@gmail.com>
2017-01-19 14:55:51 -0500

    add remaining selectors

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const micro = require('micro')
 const xray = require('x-ray')()
 
-function cleanup (error, result) {
-  console.log(result)
+const selectors = {
+  full: 'div:nth-child(1) a',
+  href: 'div:nth-child(1) a@href',
+  desc: 'div:nth-child(3) p',
+  stars: 'div:nth-child(4) [aria-label="Stargazers"]',
+  forks: 'div:nth-child(4) [aria-label="Forks"]',
+  today: 'div:nth-child(4) .float-right'
 }
 
-async function scrape (req, res) {
-  xray('https://github.com/trending/javascript', '.repo-list li', [{
-    href: 'div:nth-child(1) a@href',
-    desc: 'div:nth-child(3) p'
-  }])(cleanup)
+const cleanup = (error, result) => {
+
+}
+
+const scrape = async (req, res) => {
+  xray('https://github.com/trending/javascript', '.repo-list li', [selectors])(cleanup)
 }
 
 module.exports = scrape
