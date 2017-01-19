a6e08bb63b00d9561de60130b31781a53b722835
Michael Cavalea <callmecavs@gmail.com>
2017-01-19 14:03:29 -0500

    start xraying

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const micro = require('micro')
+const xray = require('x-ray')()
 
-module.exports = async function (req, res) {
-
+function cleanup (error, result) {
+  console.log(result)
 }
+
+async function scrape (req, res) {
+  xray('https://github.com/trending/javascript', '.repo-list li', [{
+    href: 'div:nth-child(1) a@href',
+    desc: 'div:nth-child(3) p'
+  }])(cleanup)
+}
+
+module.exports = scrape
